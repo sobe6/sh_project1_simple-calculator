d33192f6c28cb33da64f0ceab08bac7d48996ff4
Sobia_Haque <165303487+sobe6@users.noreply.github.com>
2024-06-07 19:25:01 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+
+//Asking questions feom Users through Inquirer
+
+let answer = await inquirer.prompt([
+    {message: "Enter First Number", type: "number", name: "firstNumber"},
+    {mesage: "Enter Second Number", type: "number", name: "secondNumber"},
+    {
+        message: "Select one Operator to perform Operations",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    },
+]);
+
+//conditional statement
+
+if(answer.operator === "Addition"){
+    console.log(answer.firstNumber + answer.secondNumber);
+}else if(answer.operator === "Subtraction"){
+    console.log(answer.firstNumber - answer.secondNumber);
+}else if(answer.operator === "Multiplication"){
+    console.log(answer.firstNumber * answer.secondNumber);
+}else if(answer.prompt === "Division"){
+    console.log(answer.firstNumber / answer.secondNumber);
+}
